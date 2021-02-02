28b7268ee4127270005cd8b03e25a8d3efa3e153
Ostask <502724802@qq.comy>
2021-02-02 17:21:36 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git config --global user.name "Ostask"
-git config --global user.email "502724802@qq.com"
-
+# deploy to github
+echo 'Ostask.github.io/daodao-knowledge' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:Ostask/daodao-knowledge.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://Ostask:${GITHUB_TOKEN}@github.com/Ostask/daodao-knowledge.git
+  git config --global user.name "Ostask"
+  git config --global user.email "502724802@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Ostask/daodao-knowledge.git master:gh-pages
-
-cd -
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
