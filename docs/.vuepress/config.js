80ad4f2a56da584042c721cd331098669ea164d6
Ostask <502724802@qq.com>
2023-02-08 10:54:08 +0800

    2023

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,10 +38,10 @@ module.exports = {
                         text: '基础',
                         items:[
                             {text:'javascript',link:'/pages/600fd9/'},
-                            {text:'算法&常用方法',link:'/pages/12e2d5/'}
+                            {text:'算法&常用方法',link:'/pages/12e2d5/'},
                             // {text:'ts',link:'/'},
                             // {text:'nodejs',link:'/'},
-                            // {text:'css',link:'/'}
+                            {text:'css',link:'/pages/331c3b/'}
                         ]
                     },
                     { 
